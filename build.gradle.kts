5788aafdc57a2a9d3d2ffe1b79230fbdd59c9c36
도준회 <junhoe@fastviewkorea.com>
2022-03-30 17:11:48 +0900

    maven-publish 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.beans.EventHandler.create
 
 plugins {
     id ("org.jetbrains.kotlin.jvm") version "1.5.10"
+    `maven-publish`
+}
 
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "org.brandjunhoe"
+            artifactId = "e-commerce-common-module"
+            version = "0.1"
+            from(components["java"])
+        }
+    }
 }
 
 group = "org.brandjunhoe"
@@ -18,6 +31,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
 }
 
+
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
@@ -28,4 +42,3 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
-
